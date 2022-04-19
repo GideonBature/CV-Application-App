121346e68dc16d8f77e4ce9978df647928638a5e
GideonBature <infoaboutgideon@gmail.com>
2022-04-19 06:00:11 +0100

    Configure React app for deployment to GitHub Pages

---
+++ src/components/EduExp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./EduExp.css";
+
+export default function EduInfo() {
+  return (
+    <div className="EduExp">
+      <h2>School Name:</h2>
+      <h3>Title of Study:</h3>
+      <h3>Date of Study:</h3>
+    </div>
+  );
+}
---
+++ src/components/GenInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./GenInfo.css";
+
+export default function GenInfo() {
+  return (
+    <div className="GenInfo">
+      <h1>Name:</h1>
+      <h3>Email:</h3>
+      <h3>Phone Number:</h3>
+    </div>
+  );
+}
